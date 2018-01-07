83eecba2e34e4f133087f8364df1c50a91e7b526
HONGSAVANH Julien <kyuuzuka@hotmail.fr>
2018-01-07 12:40:56 -0500

    modification tube

--- src/utilisateur/Tube.java
+++ src/utilisateur/Tube.java
@@ -5,8 +5,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-
-
 public class Tube implements Runnable {
 	private Socket socket;
 	private ObjectInputStream inputFromServer;
@@ -33,16 +31,16 @@ public class Tube implements Runnable {
 
 	public void receive() throws ClassNotFoundException, IOException {
 		inputFromServer = new ObjectInputStream(socket.getInputStream());
-		if (inputFromServer.available() > 0) {
-			Message message = (Message) inputFromServer.readObject();
+
+		Message message = (Message) inputFromServer.readObject();
+		if (message != null)
 			System.out.println(message.getMsg());
-		}
 	}
 
 	public void send(Message message) throws IOException {
 		outputToServer = new ObjectOutputStream(socket.getOutputStream());
 		outputToServer.writeObject(message);
 		outputToServer.flush();
-		
+
 	}
 }
