2cb599fec692c1f0a4c3ad61b8e7b60647cd5008
UpliftAlter <34913266+UpliftAlter@users.noreply.github.com>
2018-01-07 22:08:38 +0100

    Add JDBC dans le dossier , et Modification de Main prog pour création de BD

--- src/main/MainProg.java
+++ src/main/MainProg.java
@@ -1,10 +1,14 @@
 package main;
 
+import database.DB;
 import ihm.FrameInterface;
 
 public class MainProg {
 
 	public static void main(String[] args) {
+		
+		DB database = new DB();
+		database.creation_bd();
     	FrameInterface frame = new FrameInterface();
     	frame.setVisible(true);
     	frame.getMenuBarInterface().initFrameLogin();
